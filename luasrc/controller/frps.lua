9882a74a806b1cbe4b533e834facde719079aca4
联盟少侠 <admin@shaoxia.xyz>
2024-12-29 03:41:21 +0800

    frps 控制面板
    从 https://github.com/superzjg/luci-app-frpc_frps 拷贝

--- luasrc/controller/frps.lua
+++ luasrc/controller/frps.lua
@@ -5,6 +5,12 @@ local http = require "luci.http"
 local uci = require "luci.model.uci".cursor()
 local sys = require "luci.sys"
 
+-- 查看配置文件所需
+local e=require"nixio.fs"
+local t=require"luci.sys"
+local a=require"luci.template"
+local t=require"luci.i18n"
+
 module("luci.controller.frps", package.seeall)
 
 function index()
@@ -12,16 +18,17 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "frps"},
-		firstchild(), _("Frps")).dependent = false
+	entry({"admin", "services", "frps"}, firstchild(), _("Frps")).dependent = false
 
-	entry({"admin", "services", "frps", "common"},
-		cbi("frps/common"), _("Settings"), 1)
-
-	entry({"admin", "services", "frps", "server"},
-		cbi("frps/server"), _("Server"), 2).leaf = true
+	entry({"admin", "services", "frps", "common"}, cbi("frps/common"), _("设置"), 1)
 
 	entry({"admin", "services", "frps", "status"}, call("action_status"))
+	
+	entry({"admin", "services", "frps", "configuration"}, call("view_conf"), _("查看配置"), 3).leaf = true
+	
+	entry({"admin", "services", "frps", "get_log"}, call("get_log")).leaf = true
+	entry({"admin", "services", "frps", "clear_log"}, call("clear_log")).leaf = true
+	entry({"admin", "services", "frps", "log"}, cbi("frps/log"), _("查看日志"), 6).leaf = true
 end
 
 
@@ -40,4 +47,17 @@ function action_status()
 	http.write_json({
 		running = running
 	})
+end
+
+function view_conf()
+local e=e.readfile("/var/etc/frps/frps.main.toml")or""
+a.render("frps/file_viewer",
+{title=t.translate("Frps - 查看配置文件"),content=e})
+end
+
+function get_log()
+    luci.http.write(luci.sys.exec("cat /tmp/frps_log_link.txt"))
+end
+function clear_log()
+    luci.sys.call("true > /tmp/frps_log_link.txt")
 end
